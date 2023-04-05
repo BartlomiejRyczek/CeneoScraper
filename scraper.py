05df7ab4f6f289672b2656b89313eda9351b4de1
BartlomiejRyczek <bryczek221@gmail.com>
2023-04-05 16:35:35 +0200

    2023-04-05 Bartlomiej Ryczek

--- scraper.py
+++ scraper.py
@@ -8,4 +8,6 @@ product_code = "87884295"
 url = f"https://www.ceneo.pl/{product_code}#tab=reviews"
 response = requests.get(url)
 page_dom = BeautifulSoup(response.text, "html.parser")
-print(page_dom.prettify())
+opinions = page_dom.select("div.js_product-review")
+for opinion in opinions:
+    print(type(opinion))
